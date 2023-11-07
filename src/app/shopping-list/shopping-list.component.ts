e1e8d3388911129230d5917d89b6032e939d07ba
Swapnil Potdar <potdar.swapnil10@gmail.com>
2023-11-07 20:37:13 +0530

    shopping list service added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,16 +9,13 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
+  ingredients: Ingredient[];
 
-  constructor() {
+  constructor(private slService: ShoppingListService) {
 
   }
   ngOnInit() {
-
+    this.ingredients = this.slService.getIngredients();
   }
 
   onIngredientAdded(ingredient: Ingredient) {
