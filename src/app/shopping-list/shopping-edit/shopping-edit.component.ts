8520632b036429a68909ea0c5304ea05ec2e0eb4
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-01-16 22:59:59 +0530

    loading shopping list items into the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnDestroy,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NgForm } from '@angular/forms';
@@ -17,17 +18,26 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit,OnDestroy {
+  @ViewChild('f') slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
    this.subscription = this.slService.startedEditing
     .subscribe(
+      (index:number)=>{
       this.editedItemIndex= index;
       this.editMode= true;
+      this.editedItem= this.slService.getIngredients(index);
+      this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+      }
     );
   }
 
