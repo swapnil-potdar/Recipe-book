4dde7cca0002493373a90be0f7b8d82bab713260
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-01-21 23:01:33 +0530

    form reset

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -57,6 +57,11 @@ export class ShoppingEditComponent implements OnInit,OnDestroy {
     
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode=false;
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
