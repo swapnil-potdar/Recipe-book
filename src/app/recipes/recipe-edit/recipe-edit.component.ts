970cc677d3813116b7eaf13f019f4285b7beaec7
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-01-26 23:21:44 +0530

    creating form for editing recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
+import { FormGroup,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,8 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params
@@ -20,6 +24,24 @@ export class RecipeEditComponent implements OnInit {
           this.editMode = params['id'] != null;
         }
       );
+  }
+  private initForm() {
+    let recipeName='';
+    let recipeImagePath='';
+    let recipeDescription='';
+
+    if(this.editMode){
+      const recipe=this.recipeService.getRecipe(this.id);
+      recipeName=recipe.name;
+      recipeImagePath=recipe.imagePath;
+      recipeDescription=recipe.description;
+    }
+
+    this.recipeForm=new FormGroup({
+        'name': new FormControl(recipeName),
+        'imagePath': new FormControl(recipeImagePath),
+        'description': new FormControl(recipeDescription)
+    });
   }
 
 }
