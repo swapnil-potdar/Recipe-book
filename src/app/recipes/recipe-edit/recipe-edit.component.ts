4a22e5ac82c53458822ffe93d280f9dcabaf6580
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-01-28 23:52:16 +0530

    syncing form with HTML

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -25,6 +25,11 @@ export class RecipeEditComponent implements OnInit {
         }
       );
   }
+
+  onSubmit(){
+    console.log(this.recipeForm);
+              }
+              
   private initForm() {
     let recipeName='';
     let recipeImagePath='';
