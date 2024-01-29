ce15eeb3e3b1704ea5bf82477ca80493c605e888
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-01-29 23:21:28 +0530

    adding ingredient controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -34,18 +34,31 @@ export class RecipeEditComponent implements OnInit {
     let recipeName='';
     let recipeImagePath='';
     let recipeDescription='';
+    let recipeIngredients=new FormArray([]);
 
     if(this.editMode){
       const recipe=this.recipeService.getRecipe(this.id);
       recipeName=recipe.name;
       recipeImagePath=recipe.imagePath;
       recipeDescription=recipe.description;
+      if (recipe['ingredients']){
+        for(let ingredient of recipe.ingredients){
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm=new FormGroup({
         'name': new FormControl(recipeName),
         'imagePath': new FormControl(recipeImagePath),
-        'description': new FormControl(recipeDescription)
+        'description': new FormControl(recipeDescription),
+        'ingredients': recipeIngredients
+
     });
   }
 
