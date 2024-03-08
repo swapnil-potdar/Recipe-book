33da3885c5e738146dbfa27a010cef7344047269
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-03-08 23:22:53 +0530

    creating backend

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,35 +1,3 @@
-/* import { EventEmitter, Injectable } from "@angular/core";
-import { Recipe } from "./recipe.model";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
-
-@Injectable()
-export class RecipeService {
-
-    recipeSelected=new EventEmitter<Recipe>();
-
-    recipes: Recipe[] = [
-        new Recipe('samosa',
-        'an indian snack',
-        'https://c.ndtvimg.com/2023-03/0m65kep_samosa_625x300_10_March_23.jpg',
-        [
-            new Ingredient('potatoes',1),
-            new Ingredient('sauce',2),
-        ])
-      ];
-
-    constructor(private slService: ShoppingListService){}  
-    
-    getRecipes(){
-        return this.recipes.slice();
-    }
-
-    addIngredientsToShoppingList(ingredients : Ingredient[]){
-        this.slService.addIngredients(ingredients);
-
-    }
-} */
-
 import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
@@ -45,7 +13,7 @@ export class RecipeService {
       'A super-tasty Schnitzel - just awesome!',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
-        new Ingredient('Meat', 1),
+        new Ingredient('Meat', 10),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
