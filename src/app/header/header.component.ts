67f3e76286e22fbe465b38f5b2e9d8a99e50771f
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-03-16 11:40:43 +0530

    resolving bugs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
@@ -6,14 +7,13 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
 
-  constructor(private dataStorageService: DataStorageService){}
-
-  onSaveData(){
+  onSaveData() {
     this.dataStorageService.storeRecipes();
   }
 
-  onFetchData(){
-    this.dataStorageService.fetchRecepies();
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,33 +1,43 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
-@Injectable({providedIn:'root'})
-export class DataStorageService{
-    constructor(private http: HttpClient, private recipeService: RecipeService){ }
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
-    storeRecipes(){
-        const recipes=this.recipeService.getRecipes;
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 
-        this.http.put('https://recipe-book-ab01c-default-rtdb.firebaseio.com/recipe-book.json',recipes)
-        .subscribe(response=>{
-            console.log(response);
-        });
-    }
-
-    fetchRecepies(){
-        this.http.get<Recipe[]>('https://recipe-book-ab01c-default-rtdb.firebaseio.com/recipe-book.json')
-        .pipe(map(recipes=>{
-            return recipes.map(recipe=>{
-                return {...recipe, ingredients: recipe.ingredients? recipe.ingredients:[]}
-            })
-        }))
-        .subscribe(recipes=>{
-            this.recipeService.setRecipes(recipes);
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
         })
-
-    }
+      )
+  }
 }
